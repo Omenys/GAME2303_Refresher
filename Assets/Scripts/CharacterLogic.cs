831f6b7b03c67c9ccc5f86b9fbc8d6710803bb8b
Omenys <105769289+Omenys@users.noreply.github.com>
2024-09-14 11:42:37 -0500

    Jump with New Input System

--- Assets/Scripts/CharacterLogic.cs
+++ Assets/Scripts/CharacterLogic.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterLogic : MonoBehaviour
 
@@ -8,10 +9,17 @@ public class CharacterLogic : MonoBehaviour
     [SerializeField] float speed;
     [SerializeField] Animator animator;
 
+    IA_PlayerActions playerActions;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerActions = new IA_PlayerActions();
+        playerActions.Player.Enable();
+
         rb = GetComponent<Rigidbody>();
+
+        playerActions.Player.Jump.performed += OnJump;
     }
 
     // Update is called once per frame
@@ -41,4 +49,14 @@ public class CharacterLogic : MonoBehaviour
 
         return (input.x * camRight) + (input.y * camForward);
     }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Debug.Log("Press Jump");
+            rb.AddForce(Vector3.up * 20, ForceMode.Impulse);
+        }
+    }
+
 }
